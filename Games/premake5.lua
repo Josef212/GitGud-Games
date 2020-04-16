49554681ee675c91549215ece0aa24156c8a7aa9
Josep Casanovas-Starloop <josep.casanovas@starloopstudios.com>
2020-04-16 23:29:02 +0200

    Some more about engine setup

---
+++ Games/premake5.lua
@@ -0,0 +1,109 @@
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "FlappyClone"
+	location "FlappyClone"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/GitGud/vendor/spdlog/include",
+		"%{wks.location}/GitGud/src",
+		"%{wks.location}/GitGud/vendor",
+		"%{wks.location}/GitGud/vendor/glm"
+	}
+
+	links
+	{
+		"GitGud"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			--"GG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "GG_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "GG_DIST"
+		runtime "Release"
+		optimize "on"
+
+-- -------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------
+
+project "R-Type"
+	location "R-Type"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{wks.location}/GitGud/vendor/spdlog/include",
+		"%{wks.location}/GitGud/src",
+		"%{wks.location}/GitGud/vendor",
+		"%{wks.location}/GitGud/vendor/glm"
+	}
+	
+	links
+	{
+		"GitGud"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			--"GG_PLATFORM_WINDOWS"
+		}
+	
+	filter "configurations:Debug"
+		defines "GG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		defines "GG_RELEASE"
+		runtime "Release"
+		optimize "on"
+	
+	filter "configurations:Dist"
+		defines "GG_DIST"
+		runtime "Release"
+		optimize "on"
---
+++ premake5.lua
@@ -0,0 +1,75 @@
+workspace "GitGud"
+	startproject "Sandbox"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+group "Dependencies"
+	include "GitGud/vendor/glfw"
+	include "GitGud/vendor/glad"
+	include "GitGud/vendor/imgui"
+	
+group "Games"
+	include "Games"
+
+group ""
+	include "GitGud"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GitGud/vendor/spdlog/include",
+		"GitGud/src",
+		"GitGud/vendor",
+		"GitGud/vendor/glm"
+	}
+
+	links
+	{
+		"GitGud"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			--"GG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "GG_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "GG_DIST"
+		runtime "Release"
+		optimize "on"
